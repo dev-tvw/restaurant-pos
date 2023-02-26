91995b611036b466331b75abceea33db6635d83c
virtualsiraj <siraj@shopini.com>
2023-02-26 18:56:33 +0500

    commit for login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,17 @@
 <x-guest-layout>
    <section class="login-content">
       <div class="row m-0 align-items-center bg-white vh-100">
-         <div class="col-md-6">
+         <div class="col-md-8">
             <div class="row justify-content-center">
                <div class="col-md-10">
                   <div class="card card-transparent shadow-none d-flex justify-content-center mb-0 auth-card">
                      <div class="card-body">
-                        <a href="{{route('dashboard')}}" class="navbar-brand d-flex align-items-center mb-3">
+                        {{-- <a href="{{route('dashboard')}}" class="navbar-brand d-flex align-items-center mb-3">
                            <img src="{{asset('images/logo.png')}}"/ height="40">
                            <h4 class="logo-title ms-3">{{env('APP_NAME')}}</h4>
                         </a>
                         <h2 class="mb-2 text-center">Sign In</h2>
-                        <p class="text-center">Login to stay connected.</p>
+                        <p class="text-center">Login to stay connected.</p> --}}
                         <x-auth-session-status class="mb-4" :status="session('status')" />
 
                         <!-- Validation Errors -->
@@ -19,31 +19,31 @@
                         <form method="POST" action="{{ route('login') }}" data-toggle="validator">
                             {{csrf_field()}}
                            <div class="row">
-                              <div class="col-lg-12">
+                              <div class="col-lg-10">
                                  <div class="form-group">
-                                    <label for="email" class="form-label">Email</label>
-                                    <input id="email" type="email" name="email"  value="{{env('IS_DEMO') ? 'admin@example.com' : old('email')}}"   class="form-control"  placeholder="admin@example.com" required autofocus>
+                                    <!-- <label for="email" class="form-label">Email</label> -->
+                                    <input style="border: 2px solid orange;" id="email" type="email" name="email"  value="{{env('IS_DEMO') ? 'admin@example.com' : old('email')}}"   class="form-control"  placeholder="Email" required autofocus>
                                  </div>
                               </div>
-                              <div class="col-lg-12">
+                              <div class="col-lg-10">
                                  <div class="form-group">
-                                    <label for="password" class="form-label">Password</label>
-                                    <input class="form-control" type="password" placeholder="********"  name="password" value="{{ env('IS_DEMO') ? 'password' : '' }}" required autocomplete="current-password">
+                                    <!-- <label for="password" class="form-label">Password</label> -->
+                                    <input style="border: 2px solid orange;" class="form-control" type="password" placeholder="Password"  name="password" value="{{ env('IS_DEMO') ? 'password' : '' }}" required autocomplete="current-password">
                                  </div>
                               </div>
-                              <div class="col-lg-6">
+                             {{-- <div class="col-lg-6">
                                  <div class="form-check mb-3">
                                     <input type="checkbox" class="form-check-input" id="customCheck1">
                                     <!-- <input type="checkbox" class="custom-control-input" id="customCheck1"> -->
                                     <label class="form-check-label" for="customCheck1">Remember Me</label>
                                  </div>
-                              </div>
+                              </div> --}}
                               {{-- <div class="col-lg-6">
                                  <a href="{{route('auth.recoverpw')}}"  class="float-end">Forgot Password?</a>
                               </div> --}}
                            </div>
-                           <div class="d-flex justify-content-center">
-                              <button type="submit" class="btn btn-primary">{{ __('Sign In') }}</button>
+                           <div class="d-flex justify-content-center col-lg-10 mt-4">
+                              <button type="submit" class="btn btn-warning">{{ __('Sign In') }}</button>
                            </div>
                            {{-- <p class="text-center my-3">or sign in with other accounts?</p>
                            <div class="d-flex justify-content-center">
@@ -82,8 +82,8 @@
                </svg>
             </div>
          </div>
-         <div class="col-md-6 d-md-block d-none bg-primary p-0 mt-n1 vh-100 overflow-hidden">
-            <img src="{{asset('images/auth/01.png')}}" class="img-fluid gradient-main animated-scaleX" alt="images">
+         <div class="col-md-4 d-md-block d-none bg-primary p-0 mt-n1 vh-100 overflow-hidden" style="background-color: #eeb78e !important; border-radius: 0px 0px 0px 120px;">
+            <img src="{{asset('images/auth/graffiti.png')}}" class="img-fluid gradient-main animated-scaleX" alt="images">
          </div>
       </div>
    </section>
