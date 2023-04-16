ab34547b54b107fcf4db996bbbe2d00c39473a84
Aroui Tarek <tareq@altatweertech.com>
2023-04-16 11:04:12 +0100

    test print page

--- resources/views/kitchen/print.blade.php
+++ resources/views/kitchen/print.blade.php
@@ -57,13 +57,13 @@
                                         <div class="text-95 col-sm-6 align-self-start d-sm-flex justify-content-end">
                                             <hr class="d-sm-none" />
                                             <div class="text-grey-m2">
-                                                <div class="mt-1 text-125" style="font-weight: 700;color:black;font-size: larger;">
+                                                <div class="mt-1 text-125" style="font-weight: 700;color:black;font-size: xx-large;">
                                                     Invoice
                                                 </div>
 
-                                                <div class="my-2" style="font-weight: 700;color:black;font-size: larger;"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">ID:</span> #{{$order->daily_code}}</div>
+                                                <div class="my-2" style="font-weight: 700;color:black;font-size: xx-large;"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">ID:</span> #{{$order->daily_code}}</div>
 
-                                                <div class="my-2" style="font-weight: 700;color:black;font-size: larger;"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Date:</span> {{dateFormat($order->created_at)}}</div>
+                                                <div class="my-2" style="font-weight: 700;color:black;font-size: xx-large;"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Date:</span> {{dateFormat($order->created_at)}}</div>
 
                                                 <!-- <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Status:</span> <span class="badge badge-warning badge-pill px-25">Unpaid</span></div> -->
                                             </div>
@@ -73,11 +73,11 @@
 
                                     <div class="mt-4">
                                         <div class="row text-600 text-white bgc-default-tp1 py-25">
-                                            <div class="d-none d-sm-block col-1" style="font-weight: 700;color:black;font-size: larger;">#</div>
-                                            <div class="col-9 col-sm-5" style="font-weight: 700;color:black;font-size: larger;">Description</div>
-                                            <div class="d-none d-sm-block col-4 col-sm-2" style="font-weight: 700;color:black;font-size: larger;">Qty</div>
-                                            <div class="d-none d-sm-block col-sm-2" style="font-weight: 700;color:black;font-size: larger;">Unit Price</div>
-                                            <div class="col-2" style="font-weight: 700;color:black">Amount</div>
+                                            <div class="d-none d-sm-block col-1" style="font-weight: 700;color:black;font-size: xx-large;">#</div>
+                                            <div class="col-9 col-sm-5" style="font-weight: 700;color:black;font-size: xx-large;">Description</div>
+                                            <div class="d-none d-sm-block col-4 col-sm-2" style="font-weight: 700;color:black;font-size: xx-large;">Qty</div>
+                                            <div class="d-none d-sm-block col-sm-2" style="font-weight: 700;color:black;font-size: xx-large;">Unit Price</div>
+                                            <div class="col-2" style="font-weight: 700;color:black;font-size: xx-large;">Amount</div>
                                         </div>
                                         <?php $total_price  = 0;
                                         $extra_price = 0; ?>
@@ -87,18 +87,18 @@
                                         ?>
                                         <div class="text-95 text-secondary-d3">
                                             <div class="row mb-2 mb-sm-0 py-25">
-                                                <div class="d-none d-sm-block col-1" style="font-weight: 700;color:black;font-size: larger;">{{$loop->iteration}}</div>
-                                                <div class="col-9 col-sm-5" style="font-weight: 700;color:black;font-size: larger;">{{$item->product->name}}
-                                                    <h4 class="mt-2" style="font-weight: 700;color:black;font-size: larger;">Extras: </h4>
+                                                <div class="d-none d-sm-block col-1" style="font-weight: 700;color:black;font-size: xx-large;">{{$loop->iteration}}</div>
+                                                <div class="col-9 col-sm-5" style="font-weight: 700;color:black;font-size: xx-large;">{{$item->product->name}}
+                                                    <h4 class="mt-2" style="font-weight: 700;color:black;font-size: xx-large;">Extras: </h4>
 
                                                     <div class="row">
                                                         @if(count($item->extras))
                                                         <div class="col-12">
                                                             <div style="background-color: gray;" class="row text-600 text-white bgc-inner-tp1 py-25">
-                                                                <div class="d-none d-sm-block col-1 text-center" style="font-weight: 700;color:black;font-size: larger;">#</div>
-                                                                <div class="col-9 col-sm-5 text-center" style="font-weight: 700;color:black;font-size: larger;">Name</div>
-                                                                <div class="d-none d-sm-block col-2 col-sm-2 text-center" style="font-weight: 700;color:black;font-size: larger;">Qty</div>
-                                                                <div class="d-none d-sm-block col-sm-4 text-center" style="font-weight: 700;color:black;font-size: larger;">Unit Price</div>
+                                                                <div class="d-none d-sm-block col-1 text-center" style="font-weight: 700;color:black;font-size: xx-large;">#</div>
+                                                                <div class="col-9 col-sm-5 text-center" style="font-weight: 700;color:black;font-size: xx-large;">Name</div>
+                                                                <div class="d-none d-sm-block col-2 col-sm-2 text-center" style="font-weight: 700;color:black;font-size: xx-large;">Qty</div>
+                                                                <div class="d-none d-sm-block col-sm-4 text-center" style="font-weight: 700;color:black;font-size: xx-large;">Unit Price</div>
                                                             </div>
                                                             @foreach($item->extras as $value)
                                                             @php
@@ -107,10 +107,10 @@
                                                             @endphp
                                                             <div class="text-95 text-secondary-d3">
                                                                 <div class="row mb-2 mb-sm-0 py-25">
-                                                                    <div class="d-none d-sm-block col-1 text-center" style="font-weight: 700;color:black;font-size: larger;">{{$loop->iteration}}</div>
-                                                                    <div class="d-none d-sm-block col-5 text-center" style="font-weight: 700;color:black;font-size: larger;">{{$value->name}}</div>
-                                                                    <div class="col-2 col-sm-2 text-center" style="font-weight: 700;color:black;font-size: larger;">{{$value->pivot->quantity}}</div>
-                                                                    <div class="d-none d-sm-block col-4 text-center" style="font-weight: 700;color:black;font-size: larger;">{{$value->price}}</div>
+                                                                    <div class="d-none d-sm-block col-1 text-center" style="font-weight: 700;color:black;font-size: xx-large;">{{$loop->iteration}}</div>
+                                                                    <div class="d-none d-sm-block col-5 text-center" style="font-weight: 700;color:black;font-size: xx-large;">{{$value->name}}</div>
+                                                                    <div class="col-2 col-sm-2 text-center" style="font-weight: 700;color:black;font-size: xx-large;">{{$value->pivot->quantity}}</div>
+                                                                    <div class="d-none d-sm-block col-4 text-center" style="font-weight: 700;color:black;font-size: xx-large;">{{$value->price}}</div>
                                                                 </div>
                                                             </div>
                                                             @endforeach
@@ -118,9 +118,9 @@
                                                         @endif
                                                     </div>
                                                 </div>
-                                                <div class="d-none d-sm-block col-2" style="font-weight: 700;color:black;font-size: larger;">{{$item->quantity}}</div>
-                                                <div class="d-none d-sm-block col-2 text-95" style="font-weight: 700;color:black;font-size: larger;">IQD {{priceformat($item->price)}}</div>
-                                                <div class="col-2 text-secondary-d2" style="font-weight: 700;color:black;font-size: larger;">{{$item->price * $item->quantity}}</div>
+                                                <div class="d-none d-sm-block col-2" style="font-weight: 700;color:black;font-size: xx-large;">{{$item->quantity}}</div>
+                                                <div class="d-none d-sm-block col-2 text-95" style="font-weight: 700;color:black;font-size: xx-large;">IQD {{priceformat($item->price)}}</div>
+                                                <div class="col-2 text-secondary-d2" style="font-weight: 700;color:black;font-size: xx-large;">{{$item->price * $item->quantity}}</div>
                                             </div>
                                         </div>
                                         @endforeach
@@ -136,24 +136,24 @@
 
                                             <div class="col-12 col-sm-5 text-grey text-90 order-first order-sm-last">
                                                 <div class="row my-2">
-                                                    <div class="col-7 text-right">
+                                                    <div class="col-7 text-right" style="font-weight: 700;color:black;font-size: xx-large;">
                                                         Extras:
                                                     </div>
                                                     <div class="col-5">
-                                                        <span class="text-120 text-secondary-d1" style="font-weight: 700;color:black;font-size: larger;">IQD {{priceformat($extra_price)}}</span>
+                                                        <span style="font-weight: 700;color:black;font-size: xx-large;">IQD {{priceformat($extra_price)}}</span>
                                                     </div>
                                                 </div>
                                                 <div class="row my-2">
-                                                    <div class="col-7 text-right" style="font-weight: 700;color:black;font-size: larger;">
+                                                    <div class="col-7 text-right" style="font-weight: 700;color:black;font-size: xx-large;">
                                                         SubTotal
                                                     </div>
                                                     <div class="col-5">
-                                                        <span class="text-120 text-secondary-d1" style="font-weight: 700;color:black">IQD {{priceformat($total_price)}}</span>
+                                                        <span style="font-weight: 700;color:black;font-size: xx-large;">IQD {{priceformat($total_price)}}</span>
                                                     </div>
                                                 </div>
 
                                                 <div class="row my-2">
-                                                    <div class="col-7 text-right" style="font-weight: 700;color:black;font-size: larger;">
+                                                    <div class="col-7 text-right" style="font-weight: 700;color:black;font-size: xx-large;">
                                                         Discount ({{$order->discount ? $order->discount : 0}}%)
                                                     </div>
                                                     <?php
@@ -166,11 +166,11 @@
                                                 </div>
 
                                                 <div class="row my-2 align-items-center bgc-primary-l3 p-2">
-                                                    <div class="col-7 text-right" style="font-weight: 700;color:black;font-size: larger;">
+                                                    <div class="col-7 text-right" style="font-weight: 700;color:black;font-size: xx-large;">
                                                         Total Amount
                                                     </div>
                                                     <div class="col-5">
-                                                        <span class="text-150 text-success-d3 opacity-2" style="font-weight: 700;color:black;font-size: larger;">IQD {{priceformat($payable)}}</span>
+                                                        <span style="font-weight: 700;color:black;font-size: xx-large;">IQD {{priceformat($payable)}}</span>
                                                     </div>
                                                 </div>
                                             </div>
@@ -179,8 +179,8 @@
                                         <hr />
 
                                         <div>
-                                            <span class="text-secondary-d1 text-105" style="font-weight: 700;color:black;font-size: larger;">Thank you for your visit</span>
-                                            <span class="float-right text-secondary-d1 text-105" style="font-weight: 700;color:black;text-align: center;font-size: larger;">By Altatweertech</span>
+                                            <span class="text-secondary-d1 text-105" style="font-weight: 700;color:black;font-size: xx-large;">Thank you for your visit</span>
+                                            <span class="float-right text-secondary-d1 text-105" style="font-weight: 700;color:black;text-align: center;font-size: xx-large;">By Altatweertech</span>
 
                                             <!-- <a href="#" class="btn btn-info btn-bold px-4 float-right mt-3 mt-lg-0">Pay Now</a> -->
                                         </div>
