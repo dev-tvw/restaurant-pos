09fb5842fc4079cba98a6f6da7c8a2146ec872b9
sadqainsaddy12 <sadqainsaddy12@gmail.com>
2023-04-20 12:32:21 +0300

    some print bugs

--- resources/views/kitchen/print.blade.php
+++ resources/views/kitchen/print.blade.php
@@ -95,6 +95,9 @@
             if ($order->discount && $order->discount > 0) {
             $discounted = ($order->discount * $total_price) / 100;
             $payable = $total_price - $discounted;
+                if ($payable % 250 !== 0) {
+                 $totalAmount = ceil($payable / 250) * 250;
+          }  
             }
         ?>
             <tr>
@@ -113,7 +116,7 @@
 
             <tr>
                 <td  colspan="2" class="total"> {{ $lang == 'en' ? "Total Amount" : "المبلغ الإجمالي" }} </td>
-                <td colspan="2">IQD {{priceformat($payable)}}</td>
+                <td colspan="2">IQD {{priceformat($totalAmount)}}</td>
             </tr> 
       </tbody>
     </table>
